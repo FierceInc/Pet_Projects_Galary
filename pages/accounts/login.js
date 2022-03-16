2a31d52766dbde73c8f24904c34ce9da77876cc2
FierceInc <fierceincii@gmail.com>
2022-03-16 22:05:10 +0200

    Added login and signup pages

--- pages/accounts/login.js
+++ pages/accounts/login.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import styles from '../../styles/Accounts.module.css'
 const Login = () => {
+    if(true) {
+        return (
+            <div className={styles.LoadingSpinner} ></div>
+            
+        )
+    }
     return (
-        <div>
-         Login   
-        </div>
+    <div className={styles.SignUp}>
+    <form className={styles.Form}>
+     <input type="email" name="email" placeholder='Email Address'/>
+     <input type="password" name="password" placeholder='Password'/>
+     <p>Forgot Password?</p>
+     <button>Login</button>
+     <h6>Do not have an account? Sign Up</h6>
+   </form>  
+   </div>
     );
 }
 
--- pages/accounts/signup.js
+++ pages/accounts/signup.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import styles from '../../styles/Accounts.module.css'
 const Signup = () => {
+    if(true) {
+        return (
+            <div className={styles.LoadingSpinner} ></div>
+            
+        )
+    }
     return (
-        <div>
-            Sign Up
-        </div>
+        <div className={styles.SignUp}>
+            <p>Welcome to My Projects Galary. Where you can showcase and share your projects with the world. No need for a portfolio or coding.</p>
+
+        <form className={styles.Form}>
+         <input type="text" name="username" placeholder='Username' autoComplete='no' />
+         <input type="email" name="email" placeholder='Email Address'/>
+         <input type="password" name="password" placeholder='Password'/>
+         <input type="password" name="passwordConfirm" placeholder='Confirm Passwword'/>
+         <button>Sign Up</button>
+         <h6>Already have an account? Login </h6>
+       </form>  
+       </div>
     );
 }
 
